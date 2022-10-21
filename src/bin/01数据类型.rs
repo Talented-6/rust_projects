0d8d6e3b376b586b38dcbc64501774ea77e89cc5
TomZz <pythongolangkotlinrust@gmail.com>
2022-10-21 17:21:11 +0800

    1

--- src/bin/01数据类型.rs
+++ src/bin/01数据类型.rs
@@ -1,4 +1,6 @@
 fn main() {
+    // # Vec数据类型
+    // Specify the capacity
     let mut num_vec = Vec::with_capacity(8); // Give it capacity 8
     num_vec.push('a'); // add one character
     println!("{}", num_vec.capacity()); // prints 8
@@ -9,4 +11,8 @@ fn main() {
     num_vec.push('a'); // add one more
     num_vec.push('a'); // add one more // Now we have 5 elements
     println!("{}", num_vec.capacity()); // Still 8
+    // Create the `Vec` using `into` method
+    let my_vec: Vec<u8> = [1, 2, 3].into();
+    let my_vec2: Vec<_> = [9, 0, 10].into();
+    println!("{:?} {:?}", my_vec, my_vec2);
 }
---
+++ src/bin/02控制流.rs
@@ -0,0 +1,38 @@
+fn match_number(input: i32) {
+    match input {
+        number @ 4 => println!("{} is an unlucky number in China (sounds close to 死)!", number),
+        number @ 13 => println!("{} is unlucky in North America, lucky in Italy! In bocca al lupo!", number),
+        _ => println!("Looks like a normal number"),
+    }
+}
+fn match_colours(rbg: (i32, i32, i32)) {
+    match rbg {
+        (r, _, _) if r < 10 => println!("Not much red"),
+        (_, b, _) if b < 10 => println!("Not much blue"),
+        (_, _, g) if g < 10 => println!("Not much green"),
+        _ => println!("Each colour has at least 10"),
+    }
+}
+fn main() {
+    // # match
+    // normal
+    let my_number: u8 = 5;
+    match my_number {
+        0 => println!("it's zero"),
+        1 => println!("it's one"),
+        2 => println!("it's two"),
+        _ => println!("It's some other number"),
+    }
+    // @
+    match_number(50);
+    match_number(13);
+    match_number(4);
+    // if
+    let first = (200, 0, 0);
+    let second = (50, 50, 50);
+    let third = (200, 50, 0);
+
+    match_colours(first);
+    match_colours(second);
+    match_colours(third);
+}
