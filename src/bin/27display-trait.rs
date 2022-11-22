8d0421e045f45743d9d5e0ba16ed72cc7985d4e2
= <=>
2022-11-22 14:49:19 +0800

    colors

--- src/bin/27display-trait.rs
+++ src/bin/27display-trait.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code)]
+use colored::{self, Colorize};
 use std::fmt::{self, Display};
 #[derive(Debug, PartialEq)]
 enum FileState {
@@ -14,14 +15,14 @@ struct File {
 impl Display for FileState {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            FileState::Open => write!(f, "Open"),
-            FileState::Closed => write!(f, "Closed"),
+            FileState::Open => write!(f, "{}", "Open".green().bold()),
+            FileState::Closed => write!(f, "{}", "Closed".red().bold()),
         }
     }
 }
 impl Display for File {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "File: {} is {}", self.name, self.state)
+        write!(f, "File: {} is {}", self.name.cyan().italic(), self.state)
     }
 }
 impl File {
@@ -36,5 +37,5 @@ impl File {
 fn main() {
     let mut f = File::new("hello.txt");
     f.state = FileState::Open;
-    println!("{}\n{}", f, f.state)
+    println!("{}", f);
 }
